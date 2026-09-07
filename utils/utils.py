fad73761aa355a46a055f26782e584d9657049a3
Reinhart <196166520+Reinhart-py@users.noreply.github.com>
2026-09-07 12:18:00 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,3 @@
-def build_search_query(city_name: str, query: str) -> str:
-    SEARCH_QUERY_STRING: str = (
-        f"https://2gis.ae/{city_name.lower()}/search/{query.title()}?m"
-    )
-
-    return SEARCH_QUERY_STRING
+def build_search_query(city_name: str, query: str, country_tld: str = "ae") -> str:
+    tld = country_tld.strip().lstrip(".")
+    return f"https://2gis.{tld}/{city_name.lower()}/search/{query.title()}?m"
