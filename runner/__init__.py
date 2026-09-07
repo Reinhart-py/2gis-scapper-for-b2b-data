3450383bbe8d02806b43da112a00102a497fe85a
Reinhart <196166520+Reinhart-py@users.noreply.github.com>
2026-09-07 12:19:10 +0530

    Update __init__.py

--- runner/__init__.py
+++ runner/__init__.py
@@ -1,3 +1,8 @@
-from .runner import Runner
-
-__all__ = ["Runner"]
+def __init__(self, config: Namespace) -> None:
+        country_code = getattr(config, "country", "ae")
+        self.search_query: str = build_search_query(
+            city_name=config.city_name,
+            query=config.query_string,
+            country_tld=country_code,
+        )
+        self.output_dir: str = config.output_path
