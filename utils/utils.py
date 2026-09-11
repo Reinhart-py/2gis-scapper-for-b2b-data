0ae24c6086c73d263cdd9abb574eb15bd4c106fa
Reinhart <196166520+Reinhart-py@users.noreply.github.com>
2026-09-11 17:36:07 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -4,5 +4,5 @@ def build_search_query(
     tld = country_tld.strip().lstrip(".")
     base_url = f"https://2gis.{tld}/{city_name.lower()}/search/{query.title()}"
     if page > 1:
-        return f"{base_url}/page/{page}?m"
+        return f"{base_url}/page/{page}?m=54.756359%2C24.560894%2F9.96"
     return f"{base_url}?m"
